9fad7a45e54337428679240f5198da76dee91adc
weihung_lai <u9908028@gmail.com>
2022-04-14 18:36:05 +0800

    bootstrap test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';//この行
+import { Button } from 'react-bootstrap';//この行
+//インストールは npm react-bootstrap bootstrap 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>TEST Button</Button>
     </div>
   );
 }
